44a18d0167a06d3d81679c44c6804b778f6412a2
Tony V <vaninanton@gmail.com>
2022-12-19 16:04:00 +0600

    v0.0.2

--- src/TelegramChannelHandler.php
+++ src/TelegramChannelHandler.php
@@ -11,9 +11,9 @@ use Monolog\Logger;
 
 class TelegramChannelHandler extends AbstractProcessingHandler
 {
-    private string $appName;
-    private string $appEnv;
     private TelegramService $telegramService;
+
+    private string $text = '';
 
     /**
      * TelegramHandler constructor.
@@ -23,33 +23,40 @@ class TelegramChannelHandler extends AbstractProcessingHandler
     public function __construct(array $config)
     {
         $monologLevel = Logger::toMonologLevel($config['level']);
-        parent::__construct($monologLevel);
-
-        $this->appName = config('app.name');
-        $this->appEnv = config('app.env');
 
         $this->telegramService = new TelegramService(
             config('telegram-channel-logger.bot_token'),
             config('telegram-channel-logger.chat_id')
         );
+        parent::__construct($monologLevel);
+
+        $templateHeader = config('telegram-channel-logger.template_header');
+        $header = view($templateHeader, [
+            'appName' => config('app.name'),
+            'appEnv' => config('app.env'),
+        ])->render();
+        $this->text .= $header.PHP_EOL;
     }
 
     /**
      * Send log text to Telegram
      *
-     * @param array $record
+     * @param  array  $record
      * @return void
+     *
      * @throws GuzzleException
      */
     protected function write(array $record): void
     {
-        $template = config('telegram-channel-logger.template');
+        try {
+            $template = config('telegram-channel-logger.template');
+            $message = view($template, ['message' => 'test'])->render();
+            $this->text .= $message.PHP_EOL;
 
-        $message = view($template, array_merge($record, [
-            'appName' => $this->appName,
-            'appEnv'  => $this->appEnv,
-        ]))->render();
-
-        $this->telegramService->sendMessage($message);
+            $this->telegramService->sendMessage($this->text);
+        } catch (\Throwable $th) {
+            dd($th);
+            //throw $th;
+        }
     }
 }
--- src/views/default.blade.php
+++ src/views/default.blade.php
@@ -1,9 +1,1 @@
-<b>{{ $appName }}</b> ({{ $level_name }})
-Env: {{ $appEnv }}
----
-{!! $message !!}
----
-@if ($context)
-<b>Context:</b>
-<code>@json($context, JSON_PRETTY_PRINT)</code>
-@endif
+[{{ now()->toDateTimeString() }}] {!! $message !!}
